e7ade933d8d70678c6a9872134b23e78351076f0
MarvinChacon <mcrojas@virionstrategies.com>
2016-05-06 23:23:57 -0600

    el logo de tiqueso ahora envia a admin general. El nombre ahora enlaza al perfil

--- resources/views/plantillas/general/cabecera_nav.blade.php
+++ resources/views/plantillas/general/cabecera_nav.blade.php
@@ -1,6 +1,6 @@
 <header class="main-header">
     <!-- Logo -->
-    <a href="/perfil" class="logo">
+    <a href="/admin_general" class="logo">
         <!-- mini logo for sidebar mini 50x50 pixels -->
         <span class="logo-mini"><b>{{ Config::get("app.nombre_app_abreviada")  }}</b></span>
         <!-- logo for regular state and mobile devices -->
--- resources/views/plantillas/general/usuario_sidebar.blade.php
+++ resources/views/plantillas/general/usuario_sidebar.blade.php
@@ -3,7 +3,7 @@
         <img src="{{ Auth::user()->obtenerFoto()  }}" class="img-circle" alt="User Image">
     </div>
     <div class="pull-left info">
-        <p>{{ Auth::user()->nombre  }} {{ Auth::user()->apellido  }}</p>
+        <p><a href="/perfil">{{ Auth::user()->nombre  }} {{ Auth::user()->apellido  }}</a></p>
         <!-- <a href="#"><i class="fa fa-circle text-success"></i> Online</a> -->
     </div>
 </div>
