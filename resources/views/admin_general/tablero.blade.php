c88759029dec6ca5d5510670749f50149c3759c3
Danilo Ramirez <daniloramirez.cr@gmail.com>
2016-05-12 22:18:50 -0600

    rototipo de graficos tablero principal

--- resources/views/admin_general/tablero.blade.php
+++ resources/views/admin_general/tablero.blade.php
@@ -69,6 +69,25 @@
     </div><!-- ./col -->
 </div><!-- /.row -->
 
+<!-- Gráficos -->
+<div class="row">
+    <div class="col-xs-12">
+        <div class="nav-tabs-custom">
+            <!-- Tabs within a box -->
+            <ul class="nav nav-tabs pull-right">
+                <li class="active"><a href="#revenue-chart" data-toggle="tab">{{ "Gráfico lineal"  }}</a></li>
+                <li><a href="#sales-chart" data-toggle="tab">{{ "Gráfico Circular"  }}</a></li>
+                <li class="pull-left header"><i class="fa fa-inbox"></i> {{ "Entrada y Salida de Productos (Prototipo)"  }}</li>
+            </ul>
+            <div class="tab-content no-padding">
+                <!-- Morris chart - Sales -->
+                <div class="chart tab-pane active" id="revenue-chart" style="position: relative; height: 300px;"></div>
+                <div class="chart tab-pane" id="sales-chart" style="position: relative; height: 300px;"></div>
+            </div>
+        </div><!-- /.nav-tabs-custom -->
+    </div>
+</div>
+
 <div class="row">
 
     <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
@@ -116,10 +135,6 @@
                 <a href="/admin_procesos/ver" class="btn btn-sm btn-default btn-flat pull-right">{{ "Ver todos los procesos activos"  }}</a>
             </div><!-- /.box-footer -->
         </div><!-- /.box -->
-
-
-
-
 
     </div><!-- /.col -->
 
@@ -177,3 +192,46 @@
 <!-- =========================================================== -->
 
     @stop
+
+@section("extra_js")
+
+    <script type="text/javascript">
+        $( document).ready(function(){
+            // Sales chart
+            var area = new Morris.Area({
+                element: 'revenue-chart',
+                resize: true,
+                data: [
+                    {y: '2011 Q1', item1: 2666, item2: 2666},
+                    {y: '2011 Q2', item1: 2778, item2: 2294},
+                    {y: '2011 Q3', item1: 4912, item2: 1969},
+                    {y: '2011 Q4', item1: 3767, item2: 3597},
+                    {y: '2012 Q1', item1: 6810, item2: 1914},
+                    {y: '2012 Q2', item1: 5670, item2: 4293},
+                    {y: '2012 Q3', item1: 4820, item2: 3795},
+                    {y: '2012 Q4', item1: 15073, item2: 5967},
+                    {y: '2013 Q1', item1: 10687, item2: 4460},
+                    {y: '2013 Q2', item1: 8432, item2: 5713}
+                ],
+                xkey: 'y',
+                ykeys: ['item1', 'item2'],
+                labels: ['Item 1', 'Item 2'],
+                lineColors: ['#a0d0e0', '#3c8dbc'],
+                hideHover: 'auto'
+            });
+
+            //Donut Chart
+            var donut = new Morris.Donut({
+                element: 'sales-chart',
+                resize: true,
+                colors: ["#3c8dbc", "#f56954", "#00a65a"],
+                data: [
+                    {label: "Download Sales", value: 12},
+                    {label: "In-Store Sales", value: 30},
+                    {label: "Mail-Order Sales", value: 20}
+                ],
+                hideHover: 'auto'
+            });
+        });
+    </script>
+@stop
