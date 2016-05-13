fd616927aab7ab217e23991663589b8c96bab368
Danilo Ramirez <daniloramirez.cr@gmail.com>
2016-05-12 22:20:07 -0600

    rototipo de graficos tablero principal

--- resources/views/plantillas/secadmin/footerjs.blade.php
+++ resources/views/plantillas/secadmin/footerjs.blade.php
@@ -9,7 +9,7 @@
 <!-- Bootstrap 3.3.5 -->
 <script src="/bootstrap/js/bootstrap.min.js"></script>
 <!-- Morris.js charts -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+<script src="/js/raphael-min.js"></script>
 <script src="/plugins/morris/morris.min.js"></script>
 <!-- Sparkline -->
 <script src="/plugins/sparkline/jquery.sparkline.min.js"></script>
